6e2bb4882516219e1244eb1d694038a251351c5a
eric19961102 <eric19961102@gmail.com>
2025-02-05 11:50:14 +0800

    feat: beta online pm2 start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,5 +18,26 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3001
       }
+    },
+    {
+      name: "xaiagentplatform",     // 开发/测试环境
+      script: "server.js",              
+      instances: 1,
+      exec_mode: "fork",
+      watch: true,                      
+      ignore_watch: ["node_modules", "logs"], 
+      max_memory_restart: "1G",
+      node_args: "--max-old-space-size=1024",
+      listen_timeout: 50000,
+      kill_timeout: 5000,
+      max_restarts: 5,
+      min_uptime: "5m",
+      source_map_support: true,         
+      autorestart: true,
+      exp_backoff_restart_delay: 100,
+      env: {
+        NODE_ENV: "production",             
+        PORT: 3000
+      }
     }]
   } 
