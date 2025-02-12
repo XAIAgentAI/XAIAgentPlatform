e684bcffe6c04bc855bd683cc3405827ea7311c0
eric19961102 <eric19961102@gmail.com>
2025-02-12 17:44:01 +0800

    style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -130,7 +130,7 @@ const Navbar = () => {
 
   return (
     <nav className="sticky top-0 left-0 right-0 w-full bg-background z-50">
-      <div className="container mx-auto lg:h-18 h-16 flex items-center justify-between relative">
+      <div className="w-full  lg:h-18 h-16 flex items-center justify-between relative px-8">
         {/* Logo and Navigation Links */}
         <div className="flex items-center justify-start pr-5 gap-8">
           <Link href="/" className="flex flex-shrink-0 items-center gap-2">
