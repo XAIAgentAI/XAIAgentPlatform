9c39f8fb6bd2a11afd8241d4bfeb4baec5e8d7a2
eric19961102 <eric19961102@gmail.com>
2025-02-26 14:31:42 +0800

    Revert "feat: k-line at IAO stage"
    This reverts commit ada35400193b63367cfd67991119a7d13d2005a9.

--- next.config.js
+++ next.config.js
@@ -29,10 +29,6 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    });
     return config;
   },
 }
