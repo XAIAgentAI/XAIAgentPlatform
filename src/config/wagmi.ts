18d6f726d9bccb5503ee4fee7b05746b16212dd0
eric19961102 <eric19961102@gmail.com>
2025-03-13 18:23:47 +0800

    fix: wallet modal balance is 0

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -5,14 +5,14 @@ import { currentChain, currentNetwork, dbcMainnet, dbcTestnet, testnetConfig, ma
 // 导出所有需要的配置
 export { currentChain, dbcMainnet, dbcTestnet };
 
-// wagmi 配置
-export const config = createConfig({
-  chains: [isTestnet ? dbcTestnet : dbcMainnet], // 根据环境只允许一个网络
-  connectors: [
-    injected(),
-  ],
-  transports: {
-    [dbcTestnet.id]: http(testnetConfig.rpcUrl),
-    [dbcMainnet.id]: http(mainnetConfig.rpcUrl),
-  },
-});
+// // wagmi 配置
+// export const config = createConfig({
+//   chains: [isTestnet ? dbcTestnet : dbcMainnet], // 根据环境只允许一个网络
+//   connectors: [
+//     injected(),
+//   ],
+//   transports: {
+//     [dbcTestnet.id]: http(testnetConfig.rpcUrl),
+//     [dbcMainnet.id]: http(mainnetConfig.rpcUrl),
+//   },
+// });
